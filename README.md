16913924aeaf756cc1d4bfe9c587ac02790e7c2a
J1wanSeo <jiwanseo@duck.com>
2023-03-08 23:00:18 +0900

    2023-03-08 update

--- README.md
+++ README.md
@@ -5,11 +5,16 @@
  - By these needs I made it by python(bs4) and telegram-bot-python.
 
 ## Ability
-- It crawlles [e3home](https://e3home.cau.ac.kr/em/em_1.php) [Polaris CAU](https://www.disu.or.kr/community/notice?cidx=44) [CAU NOTICE](https://www.cau.ac.kr/cms/FR_PRO_CON/BoardRss.do?pageNo=1&pagePerCnt=15&MENU_ID=100&SITE_NO=2&BOARD_SEQ=4&S_CATE_SEQ=&BOARD_TYPE=C0301&BOARD_CATEGORY_NO=&P_TAB_NO=&TAB_NO=&P_CATE_SEQ=&CATE_SEQ=&SEARCH_FLD=SUBJECT&SEARCH=') every 5 mins with my server computer.
+- It crawlles [e3home](https://e3home.cau.ac.kr/em/em_1.php) [Polaris CAU](https://www.disu.or.kr/community/notice?cidx=44) [CAU NOTICE](https://www.cau.ac.kr/cms/FR_PRO_CON/BoardRss.do?pageNo=1&pagePerCnt=15&MENU_ID=100&SITE_NO=2&BOARD_SEQ=4&S_CATE_SEQ=&BOARD_TYPE=C0301&BOARD_CATEGORY_NO=&P_TAB_NO=&TAB_NO=&P_CATE_SEQ=&CATE_SEQ=&SEARCH_FLD=SUBJECT&SEARCH=') every 1 mins with my server computer.
 - It checks with latest files
 - latest.txt contains title of article which was latest during before run.
 
 ## ToDo
-- add another website helpful to achieve higher career.
-- link with MongoDB to substitue latest.txt files.
+- [ ] add another website helpful to achieve higher career.
+- [ ] link with MongoDB to substitue latest.txt files.
  
+## edited 2023-03-08
+- deleted parameter flag which delays code
+- changed parse_format of telegram-bot to 'MarkdownV2'
+- bot automatically gets articles body by entering article's address at POLARIS CAU Notice
+- defined function name 'md2' that reconfigure texts in html to adjust it to 'Markdown Grammar'
--- main.py
+++ main.py
@@ -8,19 +8,18 @@ import re
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# python-telegram-bot 13.14 사용
-apiToken = 'Token'
-chatID = 'chatID'
+# python-telegram-bot==13.14
+apiToken = '#your_token'
+chatID = '#your_api'
 bot = telegram.Bot(token=apiToken)
 
-
-flag_1 = 0;
-flag_2 = 0;
-flag_3 = 0;
+def md2(stringex):
+    str_md2 = stringex.replace('.','\.').replace(']','\]').replace('[','\[').replace('-','\-').replace('(','\(').replace(')','\)').replace('~','\~').replace('*','\*').replace('_','\_')
+    return str_md2
 
 final = ""
 
-# 분석하려는 주소
+# url
 url = "https://e3home.cau.ac.kr/em/em_1.php"
 res = req.urlopen(url)
 url_2 = 'https://www.disu.or.kr/community/notice?cidx=44'
@@ -28,13 +27,13 @@ res_2 = req.urlopen(url_2)
 url_3 = 'https://www.cau.ac.kr/cms/FR_PRO_CON/BoardRss.do?pageNo=1&pagePerCnt=15&MENU_ID=100&SITE_NO=2&BOARD_SEQ=4&S_CATE_SEQ=&BOARD_TYPE=C0301&BOARD_CATEGORY_NO=&P_TAB_NO=&TAB_NO=&P_CATE_SEQ=&CATE_SEQ=&SEARCH_FLD=SUBJECT&SEARCH='
 res_3 = requests.get(url_3)
 
-# html 분석
+# html deparsing
 soup = BeautifulSoup(res, 'html.parser')
 soup_2 = BeautifulSoup(res_2, 'html.parser')
-soup_3 = BeautifulSoup(res_3.text, 'html.parser')
+soup_3 = BeautifulSoup(res_3.text, 'lxml-xml')
 
 
-# 원하는 내용 추출
+# extraction
 article_list = soup.select("tbody > tr")
 article_list_2 = soup_2.select("#zcmsprogram > div > table > tbody > tr")
 article_list_3 = soup_3.select("item")
@@ -46,11 +45,14 @@ latest_3 = article_list_3[0].select_one('title').text
 
 
 
-# article_list 의 최신내용 저장
+# article_list 
 with open(os.path.join(BASE_DIR, 'latest.txt'), 'r+') as f_read:
     before = f_read.readline()
     if before != latest:
-        flag_1 = 1;
+        title = article_list[0].select_one('td > div > a').get_text()
+        #date = article_list[0].select_one('td.td_datetime').get_text()
+        item = "*\[[e3](https://e3home.cau.ac.kr/em/em_1.php)\]*\n\n" +  md2(title) + "\n\n"
+        final += item
     f_read.close()
 
 with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
@@ -59,11 +61,17 @@ with open(os.path.join(BASE_DIR, 'latest.txt'), 'w+') as f_write:
 
 
         
-# article_list_2 의 최신내용 저장    
+# article_list_2     
 with open(os.path.join(BASE_DIR, 'latest_2.txt'), 'r+') as f_read:
     before = f_read.readline()
     if before != latest_2:
-        flag_2 = 1;
+        title_2 = article_list_2[0].select_one('.title > a')
+        title_2_text = title_2.get_text()
+        link_2 = 'https://www.disu.or.kr' + str(title_2['href'])
+        description_2_soup = str(BeautifulSoup(req.urlopen(link_2), 'html.parser').select_one('#printbody > div > div.fixwidth.bbs_contents')).replace('<br/>','\n').replace('<strong>','').replace('</strong>','').replace('</div>','').replace('<div class="fixwidth bbs_contents">','')
+        #date_2 = article_list_2[0].select_one('td.text-center.hidden-xs-down.FS12').get_text().replace('-','.')
+        item_2 = '*\[POLARIS\]* \n\n*[' + md2(title_2_text)+ ']' + '(' + md2(link_2) + ')*\n' + md2(description_2_soup) + '\n'
+        final += item_2
     f_read.close()
 
 with open(os.path.join(BASE_DIR, 'latest_2.txt'), 'w+') as f_write:
@@ -74,11 +82,16 @@ with open(os.path.join(BASE_DIR, 'latest_2.txt'), 'w+') as f_write:
 
     
 
-# article_list_3 의 최신내용 저장   
+# article_list_3    
 with open(os.path.join(BASE_DIR, 'latest_3.txt'), 'r+') as f_read:
     before = f_read.readline()
     if before != latest_3:
-        flag_3 = 1;
+        title_3 = article_list_3[0].select_one('title')
+        title_3_text = title_3.get_text()
+        link_3 = str(title_3['href'].replace('=','\=').replace('-','\-'))
+        description_3 = article_list_3[0].select_one('description').get_text().replace('&nbsp;','')
+        item_3 = '*\[CAU NOTICE\]* \n\n*[' + md2(title_3_text) + ']' + '(' + link_3 + ')*\n' + "\n" + md2(description_3)
+        final += item_3
     f_read.close()
 
 with open(os.path.join(BASE_DIR, 'latest_3.txt'), 'w+') as f_write:
@@ -86,31 +99,7 @@ with open(os.path.join(BASE_DIR, 'latest_3.txt'), 'w+') as f_write:
     f_write.close()
         
  
-
-if flag_1 != 0 or flag_2 != 0 or flag_3 != 0:
-    if flag_1 == 1:
-        title = article_list[0].select_one('td > div > a').get_text()
-        #date = article_list[0].select_one('td.td_datetime').get_text()
-        item = "\[[e3](https://e3home.cau.ac.kr/em/em_1.php)\]\n\n" +  title + "\n\n"
-        final += item
-
-    if flag_2 == 1:
-        title_2 = article_list_2[0].select_one('.title > a')
-        title_2_text = title_2.get_text()
-        link_2 = str(title_2['href'].replace('=','\=').replace('-','\-'))
-        #date_2 = article_list_2[0].select_one('td.text-center.hidden-xs-down.FS12').get_text().replace('-','.')
-        item_2 = '\[POLARIS\] \n\n[' + title_2_text.replace(']','\]').replace('[','\[').replace('-','\-').replace('(','\(').replace(')','\)').replace('~','\~')+ ']' + '(' + 'https://www\.disu\.or\.kr/'  + link_2 + ')\n'
-        final += item_2
-
-    if flag_3 == 1:
-        title_3 = article_list_3[0].select_one('title')
-        title_3_text = title_3.get_text()
-        link_3 = str(title_3['href'].replace('=','\=').replace('-','\-'))
-        item_3 = '\[CAU NOTICE\] \n\n[' + title_3_text.replace(']','\]').replace('[','\[').replace('-','\-').replace('(','\(').replace(')','\)').replace('~','\~') + ']' + '(' + link_3 + ')\n'
-        final += item_3
-        #print(final)
-
-    # send_to_telegram(str(final))
+if len(final) != 0:
     bot.sendMessage(chat_id = chatID, text= "\n" + final + "\n", parse_mode = 'MarkdownV2', disable_web_page_preview = True)
-    # + "[POLARIS]" + issue_2 + "\n" + "[CAU Notice]" + issue_3 )
 
+exit()
